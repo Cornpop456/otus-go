365e77253f700dc76475eb40ba1bb2aec8717dd7
Victor Ginzburg <v-viktor456@mail.ru>
2019-12-02 21:39:13 +0300

    update now.go

--- hw1/now.go
+++ hw1/now.go
@@ -7,18 +7,23 @@ import (
 	"github.com/beevik/ntp"
 )
 
-func now() string {
+func now() (string, error) {
 	time, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+
+	if err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("Current time is %s\n", time.Format("15:04:05")), nil
+}
+
+func main() {
+	res, err := now()
 
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	return fmt.Sprintf("Current time is %s\n", time.Format("15:04:05"))
-}
-
-func main() {
-	res := now()
 	fmt.Printf(res)
 }
