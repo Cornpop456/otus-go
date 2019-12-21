5f196c6758e7256d0844cf649c793dd56b0d320a
Victor Ginzburg <v-viktor456@mail.ru>
2019-12-21 11:26:19 +0300

    add tests to Item

--- hw4/dlinked_list_test.go
+++ hw4/dlinked_list_test.go
@@ -1,7 +1,58 @@
-// package main
+package main
 
-// import (
-// 	"testing"
+import (
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// )
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValue(t *testing.T) {
+	tests := []struct {
+		i    Item
+		want interface{}
+	}{
+		{Item{}, nil},
+		{Item{value: 10}, 10},
+		{Item{value: "Hello"}, "Hello"},
+		{Item{value: []int{1, 2, 3}}, []int{1, 2, 3}},
+	}
+	for _, tc := range tests {
+		assert.Equal(t, tc.want, tc.i.Value(), "values should be equal")
+	}
+}
+
+func TestNext(t *testing.T) {
+	items := []*Item{
+		&Item{value: 10},
+		&Item{value: "hello"},
+	}
+	tests := []struct {
+		i    Item
+		want *Item
+	}{
+		{Item{}, nil},
+		{Item{next: items[0]}, items[0]},
+		{Item{next: items[1]}, items[1]},
+	}
+	for _, tc := range tests {
+		assert.Same(t, tc.want, tc.i.Next(), "pointers should be equal")
+	}
+}
+
+func TestPrev(t *testing.T) {
+	items := []*Item{
+		&Item{value: 20},
+		&Item{value: "world"},
+	}
+	tests := []struct {
+		i    Item
+		want *Item
+	}{
+		{Item{}, nil},
+		{Item{prev: items[0]}, items[0]},
+		{Item{prev: items[1]}, items[1]},
+	}
+	for _, tc := range tests {
+		assert.Same(t, tc.want, tc.i.Prev(), "pointers should be equal")
+	}
+}
--- hw4/go.mod
+++ hw4/go.mod
@@ -1,3 +1,5 @@
 module github.com/Cornpop456/otus-go/hw4
 
 go 1.13
+
+require github.com/stretchr/testify v1.4.0
