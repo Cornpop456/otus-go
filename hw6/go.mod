97536e0c3169ac8299a3401e8fbda919b4388ea8
Victor Ginzburg <v-viktor456@mail.ru>
2020-01-16 15:40:00 +0300

    implement copy

---
+++ hw6/go.mod
@@ -0,0 +1,5 @@
+module github.com/Cornpop456/otus-go/hw6
+
+go 1.13
+
+require github.com/cheggaaa/pb/v3 v3.0.4
---
+++ hw6/gocopy.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/cheggaaa/pb/v3"
+)
+
+var fromFlag string
+var toFlag string
+var offsetFlag int64
+var limitFlag int64
+
+func init() {
+	flag.StringVar(&fromFlag, "from", "", "source file")
+	flag.StringVar(&toFlag, "to", "", "dest file")
+	flag.Int64Var(&offsetFlag, "offset", 0, "offset in src file")
+	flag.Int64Var(&limitFlag, "limit", 0, "number of bytes to copy")
+
+	flag.Parse()
+}
+
+// Copy copies data from one file to another with limit and offset passed through command line args
+func Copy(from string, to string, limit int64, offset int64) error {
+	src, err := os.Open(from)
+	dst, err2 := os.Create(to)
+
+	defer src.Close()
+	defer dst.Close()
+
+	if err != nil {
+		return err
+	} else if err2 != nil {
+		return err2
+	}
+
+	fi, err := src.Stat()
+	if err != nil {
+		return err
+	}
+
+	srcSize := fi.Size()
+
+	if srcSize-offset <= 0 {
+		return fmt.Errorf("Incorrect offset: out of file size")
+	} else if (srcSize-offset) < limit || limit == 0 {
+		limit = srcSize - offset
+	}
+
+	src.Seek(offsetFlag, 0)
+
+	tmpl := `{{green "Progress status:" }} {{percent . | magenta}} {{green "copied"}}`
+
+	bar := pb.ProgressBarTemplate(tmpl).Start64(limit)
+
+	barReader := bar.NewProxyReader(src)
+
+	if _, err := io.CopyN(dst, barReader, limit); err != nil {
+		return err
+	}
+
+	bar.Finish()
+	return nil
+}
+
+func main() {
+	err := Copy(fromFlag, toFlag, limitFlag, offsetFlag)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
