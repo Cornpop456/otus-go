00bf7ebb2e86332f696625f3cf39b6fcd9fadd73
Victor Ginzburg <v-viktor456@mail.ru>
2020-01-31 12:40:40 +0300

    add configuration and hello server

--- calendar-app/cmd/app/main.go
+++ calendar-app/cmd/app/main.go
@@ -1,21 +1,65 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/Cornpop456/otus-go/calendar-app/internal/calendar"
 	"github.com/Cornpop456/otus-go/calendar-app/internal/calendar/memstorage"
+
+	"gopkg.in/yaml.v2"
+)
+
+// Config struct for configuring app
+type Config struct {
+	HTTPListen string `yaml:"http_listen"`
+	LogFile    string `yaml:"log_file"`
+	LogLevel   string `yaml:"log_level"`
+}
+
+var (
+	configPath string
+	config     Config
+	logFile    *os.File
 )
 
 func init() {
-	log.SetPrefix("LOG: ")
+	flag.StringVar(&configPath, "config", "", "path to config file")
+	flag.Parse()
+
+	yamlFile, err := ioutil.ReadFile(configPath)
+
+	if err != nil {
+		panic("Can not read config file!")
+	}
+
+	err = yaml.Unmarshal(yamlFile, &config)
+
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
+	}
+
+	logFile, err = os.OpenFile(config.LogFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	log.SetOutput(logFile)
+	log.SetPrefix(config.LogLevel + ": ")
 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 }
 
 func main() {
+	defer logFile.Close()
+
 	calendar := calendar.New(memstorage.NewEventsLocalStorage())
+
 	t, err := time.Parse(time.RFC822, "26 Jan 20 19:00 MSK")
 	if err != nil {
 		log.Fatalln(err)
@@ -26,7 +70,7 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	if _, err := calendar.AddEvent("Go to university", "learning in university...", t2); err != nil {
+	if _, err := calendar.AddEvent("Do cooking", "cook dinner", t2); err != nil {
 		log.Fatalln(err)
 	}
 
@@ -34,9 +78,12 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	events := calendar.GetEvents()
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%+v\n", *r)
+		for _, v := range calendar.GetEvents() {
+			fmt.Fprintf(w, "Event: %s\nDescription: %s\nDate: %s\n\n", v.Name, v.Description, v.EventDate.String())
+		}
+	})
 
-	for _, v := range events {
-		fmt.Println(v)
-	}
+	http.ListenAndServe(config.HTTPListen, nil)
 }
--- calendar-app/go.mod
+++ calendar-app/go.mod
@@ -2,4 +2,7 @@ module github.com/Cornpop456/otus-go/calendar-app
 
 go 1.13
 
-require github.com/stretchr/testify v1.4.0
+require (
+	github.com/stretchr/testify v1.4.0
+	gopkg.in/yaml.v2 v2.2.2
+)
